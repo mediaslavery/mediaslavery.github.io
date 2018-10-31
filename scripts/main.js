fc95193eede37011a21b616e9a1b1c3bd50d3dbc
mediaslavery <43820237+mediaslavery@users.noreply.github.com>
2018-10-30 20:13:04 -0400

    testing video player

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,32 @@ $(document).ready(function() {
   }
 });
 });
+
+// POPUP VIDEO WITH INSTAGRAM
+const markup = `<div class="mfp-iframe-container">
+     <div class="mfp-close"></div>
+     <iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>
+   </div>`;
+
+$('.popup-instagram-mf').magnificPopup({
+  type: iframe,
+  iframe: {
+    markup,
+    patterns: {
+      instagram: {
+        index: 'instagram.com',
+        id: url => {
+          const m = url.match(/(?:https?:\/\/(?:www\.)?)?instagram\.com\/p\/(\w+)/);
+          if (!m || !m[1]) {
+            return null;
+          }
+
+          return m[1];
+        },
+        src: '//instagram.com/p/%id%/embed/',
+      },
+      ...
+    },
+  },
+  ...
+}); // END INSTAGRAM MAGNIFIC POPUP
