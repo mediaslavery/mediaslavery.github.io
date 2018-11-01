5d780bfb69ec0eeaa2863af45565c2b7297cc8cd
mediaslavery <43820237+mediaslavery@users.noreply.github.com>
2018-11-01 12:40:50 -0400

    another popup test

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Video pop-up //
 $(document).ready(function() {
   $('.video-gallery').magnificPopup({
   delegate: 'a', // child items selector, by clicking on it popup will open
@@ -7,4 +8,10 @@ $(document).ready(function() {
     enabled:true
   }
 });
+});
+
+// Web pop-up//
+$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 });
