8b88537b85fc3972f5fea4fe42191e5bea1a59c7
mediaslavery <43820237+mediaslavery@users.noreply.github.com>
2018-11-07 12:05:34 -0500

    Massive update. Nearly Complete.

--- js/index.js
+++ js/index.js
@@ -9,9 +9,3 @@ $(document).ready(function() {
   }
 });
 });
-
-// Web pop-up//
-$('.open-popup-link').magnificPopup({
-  type:'inline',
-  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-});
